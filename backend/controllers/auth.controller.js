eb6145f17a2df1b5a26a68db02a756e961d93206
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-04-04 00:26:46 +0200

    ..

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,24 +2,32 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 
+// create a new user
 export const signupUser = async (req, res) => {
   const { username, fullName, password, confirmPassword, gender } = req.body;
   try {
+    // check if the user has entered all the fields
     if (password !== confirmPassword) {
       return res.status(400).json({ message: "passwords do not match" });
     }
+    // check if the username already exists
     const user = await User.findOne({ username: username });
+
+    // if the username already exists, return an error message
     if (user) {
       return res.status(400).json({ message: "username already exists" });
     }
 
+    // create a profile picture for the user
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
     const girlProfilePic = `https://avatar.iran.liara.run/public/gil?username=${username}`;
     const othersProfilePic = `https://avatar.iran.liara.run/username?username=${username}+${fullName}`;
 
+    // hash the password
     const salt = await bcrypt.genSalt(12);
     const hashedPassword = await bcrypt.hash(password, salt);
 
+    // create a new user
     const newUser = new User({
       username: username,
       fullName: fullName,
@@ -27,10 +35,13 @@ export const signupUser = async (req, res) => {
       profilePic: gender === "male" ? boyProfilePic : gender === "female" ? girlProfilePic : othersProfilePic,
       gender: gender,
     });
-
+    // save the user to the database
     await newUser.save();
+
+    // get all the users
     const getAllUsers = await User.find();
 
+    // return a success message to the user
     res.status(201).json({
       message: "user created successfully",
       user: { getAllUsers },
